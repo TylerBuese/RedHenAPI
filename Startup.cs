e3394618b2c12d7c8667fd8815381059c898fbb8
bzb0y <tylerbuese@gmail.com>
2022-02-25 20:08:41 -0600

    changes

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace RedHenAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RedHenAPI v1"));
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
